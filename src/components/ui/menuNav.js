c40f12c7449afbe655679c7b0de56e2e88e3ffb1
ummfieg <ummfieg@naver.com>
2025-11-17 16:16:44 +0900

    feat(menuNav):공통 메뉴 nav bar 구현

---
+++ src/components/ui/menuNav.js
@@ -0,0 +1,15 @@
+export default function MenuNav() {
+  return (
+    <>
+      <div className="flex gap-3 justify-end items-center text-sub">
+        <span className="cursor-pointer">home</span>
+        <div>|</div>
+        <span className="cursor-pointer">register</span>
+        <div>|</div>
+        <span className="cursor-pointer">study</span>
+        <div>|</div>
+        <span className="cursor-pointer">my types</span>
+      </div>
+    </>
+  );
+}
