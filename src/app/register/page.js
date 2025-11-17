a572b6d1f13a728daeb86cdd0bac98dfb12f0586
ummfieg <ummfieg@naver.com>
2025-11-17 16:18:34 +0900

    feat(resigter page, word): register페이지 및 word 컴포넌트 구현

---
+++ src/app/register/page.js
@@ -0,0 +1,23 @@
+import Category from "@/components/ui/category";
+import NavBar from "@/components/ui/menuNav";
+import PageTitle from "@/components/ui/pageTitle";
+import Word from "@/components/register/word";
+export default function Resgister() {
+  return (
+    <>
+      <header>
+        <NavBar />
+        <div className="flex justify-between items-start my-6">
+          <Category />
+          <PageTitle />
+        </div>
+      </header>
+
+      <main className="flex justify-center my-10">
+        <div className="border-l w-full">
+          <Word />
+        </div>
+      </main>
+    </>
+  );
+}
---
+++ src/components/register/word.js
@@ -0,0 +1,25 @@
+export default function Word() {
+  return (
+    <div className="flex flex-col gap-5 py-1 px-5">
+      <span>
+        단어는 <span className="font-bold">공백 최대 3개까지</span> 허용해요.
+        <span className="text-sub ml-2">예: my type 타이핑</span>
+      </span>
+      <form className="flex flex-col gap-3">
+        <div className="grid grid-cols-5 grid-rows-6 gap-3">
+          {Array.from({ length: 30 }).map((_, index) => (
+            <input
+              key={index}
+              type="text"
+              className="border bg-main rounded-md border-transparent focus:border-b-point focus:outline-hidden px-2 py-2"
+              placeholder="단어입력"
+            />
+          ))}
+        </div>
+        <button className="cursor-pointer self-end" type="submit">
+          등록하기
+        </button>
+      </form>
+    </div>
+  );
+}
