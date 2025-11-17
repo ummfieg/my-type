c549828d205d61ef3ffeca5fb1983a2f984026a7
ummfieg <ummfieg@naver.com>
2025-11-17 19:53:47 +0900

    feat(code): register페이지 코드 입력 컴포넌트 구현

---
+++ src/components/register/code.js
@@ -0,0 +1,25 @@
+export default function Code() {
+  return (
+    <div className="flex flex-col gap-5 py-1 px-5">
+      <span>
+        코드는 <span className="font-bold">줄바꿈을 유의해서</span>{" "}
+        입력해주세요.
+      </span>
+      <form className="flex gap-3 flex-col">
+        <div className="grid grid-cols-2 gap-5">
+          {Array.from({ length: 2 }).map((_, index) => (
+            <textarea
+              key={index}
+              type="text"
+              className="border bg-main rounded-md border-transparent focus:border-b-point focus:outline-hidden px-2 py-2 resize-none h-[400px] overflow-scroll"
+              placeholder="코드 입력"
+            />
+          ))}
+        </div>
+        <button className="cursor-pointer self-end" type="submit">
+          등록하기
+        </button>
+      </form>
+    </div>
+  );
+}
