90bdf282fedfb05b995e39cf9bcb44c2caecb779
ummfieg <ummfieg@naver.com>
2025-11-18 19:44:10 +0900

    feat(page): register 페이지 카테고리별 동적 컴포넌트 추가

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,25 +1,37 @@
-import Category from "@/components/ui/category";
+"use client";
+import { useState } from "react";
+import Category from "@/components/ui/systemCategory";
 import NavBar from "@/components/ui/menuNav";
 import PageTitle from "@/components/ui/pageTitle";
 import Word from "@/components/register/word";
 import Sentence from "@/components/register/sentence";
+import Code from "@/components/register/code";
+import UserCategoryModal from "@/components/register/userCategory";
+
 export default function Resgister() {
+  const [category, setCategory] = useState("WORD");
+  const onSelect = (e) => {
+    setCategory(e);
+  };
+
   return (
     <>
       <header>
         <NavBar />
-        <div className="flex justify-between items-start my-6">
-          <Category />
+        <div className="flex justify-between items-start my-6 mx-12">
+          <Category onSelect={setCategory} category={category} />
           <PageTitle />
         </div>
       </header>
 
       <main className="flex justify-center my-10">
-        <div className="border-l w-full">
-          <Word />
-          <Sentence />
+        <div className="border-l w-4/5">
+          {category === "WORD" && <Word />}
+          {category === "SENTENCE" && <Sentence />}
+          {category === "CODE" && <Code />}
         </div>
       </main>
+      {/* <UserCategoryModal /> */}
     </>
   );
 }
