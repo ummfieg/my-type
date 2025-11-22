e249553e5a4132afca39e62fcb374236974fba45
ummfieg <ummfieg@naver.com>
2025-11-23 03:58:11 +0900

    refactor(systemCategory): clicked css 적용 및 중복코드 제거

--- src/components/ui/systemCategory.js
+++ src/components/ui/systemCategory.js
@@ -1,40 +1,36 @@
 import React from "react";
-import { HighlightButton } from "./button";
+import { ClickedButton, HighlightButton } from "./button";
+
 const categoryItems = ["WORD", "SENTENCE", "CODE"];
 
-export default function SystemCategory({ onSelect, category }) {
+const subTextList = {
+  WORD: "단어(WORD)는 한번에 30개 씩 등록할 수 있어요!",
+  SENTENCE: "문장(SENTENCE)는 한번에 5개 씩 등록할 수 있어요!",
+  CODE: "코드(CODE)는 한번에 2개 씩 등록할 수 있어요!",
+};
+
+export default function SystemCategory({ onSelect, category, pathName }) {
+  const showSubText = pathName === "/register";
   return (
     <div className="flex justify-center flex-col w-84 pt-2 ">
       <div className="rounded-full border-foreground h-14 border px-4 flex justify-around items-center ">
         {categoryItems.map((item, index) => (
-          <React.Fragment key={(item, index)}>
+          <React.Fragment key={item}>
             <HighlightButton>
-              <span
-                className="cursor-pointer text-sub "
+              <ClickedButton
+                isClicked={category === item}
                 onClick={() => onSelect(item)}
               >
                 {item}
-              </span>
+              </ClickedButton>
             </HighlightButton>
             {index < categoryItems.length - 1 && <div className="m-1">|</div>}
           </React.Fragment>
         ))}
       </div>
 
-      {category === "WORD" && (
-        <p className="my-1 px-4 text-sm">
-          단어(WORD)는 한번에 30개 씩 등록할 수 있어요!
-        </p>
-      )}
-      {category === "SENTENCE" && (
-        <p className="my-1 px-4  text-sm">
-          문장(SENTENCE)는 한번에 5개 씩 등록할 수 있어요!
-        </p>
-      )}
-      {category === "CODE" && (
-        <p className="my-1 px-4  text-sm">
-          코드(CODE)는 한번에 2개 씩 등록할 수 있어요!
-        </p>
+      {showSubText && subTextList[category] && (
+        <p className="my-1 px-4 text-sm">{subTextList[category]}</p>
       )}
     </div>
   );
